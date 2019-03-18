cd452212c4baf74d427ab41f932e108d098fbd7b
Watinguyaz <watimakhanu@gmail.com>
2019-03-18 13:01:53 +0300

    profile and user models working

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profile/')
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    contacts=models.CharField(max_length=50, blank=True)
+    bio = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.contacts
+
+    def save_user(self):
+        self.save()
+
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender,instance,created,**kwargs):
+        if created:
+            Profile.objects.create(user_id=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
